426a53b0e897cf9c15ecaa44937e51f5a1f88b7b
foko <qkrrudgh0@naver.com>
2023-06-23 15:26:12 +0900

    refactor: util패키지 구조 및 이름변경
    util패키지를 common으로 변경함

--- src/main/kotlin/com/kotlin/board/common/Constant.kt
+++ src/main/kotlin/com/kotlin/board/common/Constant.kt
@@ -1,4 +1,4 @@
-package com.kotlin.board.util
+package com.kotlin.board.common
 
 class Constant {
     companion object{
--- src/main/kotlin/com/kotlin/board/common/Result.kt
+++ src/main/kotlin/com/kotlin/board/common/Result.kt
@@ -1,4 +1,4 @@
-package com.kotlin.board.util
+package com.kotlin.board.common
 
 data class Result<T>(val data:T) {
 }
